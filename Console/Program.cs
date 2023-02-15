4633377fbf97c1baff9436d0c580b987b3dffa4d
fel88 <15663687+fel88@users.noreply.github.com>
2023-02-15 10:49:22 -0800

    init

---
+++ Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OCCTProxy proxy = new OCCTProxy();
+            proxy.InitOCCTProxy();
+            var obj1 = proxy.AddWireDraft(40);
+            var l = proxy.IteratePoly(obj1);
+            foreach (var item in l)
+            {
+                System.Console.WriteLine($"{item.X} {item.Y} {item.Z}");
+            }
+        }
+    }
+}
